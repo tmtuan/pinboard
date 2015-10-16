36ff82eefd56685b74a5c75e623ee4b38da94c5a
tmtuan <tmtuan.projects@gmail.com>
2015-10-16 16:34:00 +0700

    Basic finish

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'simple_form', '~> 3.2.0'
 
 gem 'masonry-rails', '~> 0.2.4'
 
+gem 'acts_as_votable', '~> 0.10.0'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,11 @@ Rails.application.routes.draw do
 
   resources :users
   resources :sessions
-  resources :pins
+  resources :pins do
+    member do
+      put "like", to: "pins#upvote"
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
